01bfbcacc91d91ae07ad5806a3f315290f885f0e
Balogun Ridwan <balogunridwan@gmail.com>
2019-06-24 07:50:24 +0100

    Updates

---
+++ precache-manifest.110a5458fccce6d3fb29b5730ee5f802.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac7c2341937221172d37195a732ba45",
+    "url": "/robot-friends/index.html"
+  },
+  {
+    "revision": "d75854477a6f0b193443",
+    "url": "/robot-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7b57b71e900e5c81c677",
+    "url": "/robot-friends/static/css/main.2e1f9f4e.chunk.css"
+  },
+  {
+    "revision": "d75854477a6f0b193443",
+    "url": "/robot-friends/static/js/2.42d5c8b7.chunk.js"
+  },
+  {
+    "revision": "7b57b71e900e5c81c677",
+    "url": "/robot-friends/static/js/main.b7de2b1f.chunk.js"
+  },
+  {
+    "revision": "f8c63846f1fb9c939854",
+    "url": "/robot-friends/static/js/runtime~main.88802e26.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
